ea84cb21cb772e8c819f6396099517150cd525a9
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-02-01 20:57:15 +0200

    Add event

--- JavaScript/2-abstraction.js
+++ JavaScript/2-abstraction.js
@@ -1,17 +1,27 @@
 'use strict';
 
+const { EventEmitter } = require('events');
+
 // Case 2: Active entity
 // for example: Collection, Stream, Scheduler
 
-class Collection {
+class Collection extends EventEmitter {
   constructor() {
+    super();
     this.keys = [];
     this.values = [];
   }
 
   set(key, value) {
-    this.keys.push(key);
-    this.values.push(value);
+    const index = this.keys.indexOf(key);
+    if (index === -1) {
+      this.keys.push(key);
+      this.values.push(value);
+    } else {
+      const previous = this.values[index];
+      this.values[index] = value;
+      this.emit('rewrite', key, value, previous);
+    }
   }
 
   get(key) {
@@ -24,6 +34,10 @@ class Collection {
 // Usage
 
 const collection = new Collection();
+collection.on('rewrite', (key, value, previous) => {
+  console.log('rewrite', { key, value, previous });
+});
+collection.set('name', 'Marcus');
 collection.set('name', 'Marcus Aurelius');
 const name = collection.get('name');
 console.dir({ name });
